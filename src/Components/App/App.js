bfb78f2d6fd5b4e46bf9c344276d5a489cf9a366
Reid Poole <poole.reid@gmail.com>
2023-02-01 19:27:51 -0600

    Set up Bassic APP structure

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Header from "../Header/Header";
+import Library from "../Library/Library";
+import Movie from "../Movies/Movies";
+import movieData from "../movieData/movieData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allMovies: movieData,
+    };
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <Header />
+        {/* <Library /> */}
+      </main>
+    );
+  }
 }
 
 export default App;
