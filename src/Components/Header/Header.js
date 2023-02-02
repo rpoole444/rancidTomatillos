b548e938bfc0ea4379740ff78c90f8ba52f6cf81
Reid Poole <poole.reid@gmail.com>
2023-02-01 19:29:43 -0600

    Add Basic Setup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Rancid Tomatillos</h1>
+      <input placeholder="Search" name="search" />
+      <img />
+    </header>
+  );
+};
+
+export default Header;
