bef5f2d8e5cac740c3f52aa437c41fd9c24b0fd0
Reid Poole <poole.reid@gmail.com>
2023-02-01 20:05:20 -0600

    Add Library Setup

---
+++ src/Components/Library/Library.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Movies from "../Movies/Movies";
+import "./Library.css";
+
+const Library = ({ allMovies }) => {
+  const filteredMovies = allMovies.map((movie) => {
+    console.log(movie);
+    return (
+      <Movies
+        id={movie.id}
+        key={movie.id}
+        poster_path={movie.poster_path}
+        backdrop_path={movie.backdrop_path}
+        title={movie.title}
+        average_rating={movie.average_rating}
+        release_date={movie.release_date}
+      />
+    );
+  });
+  return <div className="movie-container">{filteredMovies}</div>;
+};
+
+export default Library;
