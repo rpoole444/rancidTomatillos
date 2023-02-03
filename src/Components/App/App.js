2ca5b2ceaa2b37142b885268208d4c958978ed14
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-02 19:44:10 -0600

    Add componentDidMount feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,25 @@
 import { Component } from "react";
+import { fetchApiUrl } from "./Api";
 import Header from "../Header/Header";
 import Library from "../Library/Library";
-import Movies from "../Movies/Movies";
-import movieData from "../movieData/movieData";
-
+// import Movies from "../Movies/Movies";
+// import movieData from "../movieData/movieData";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      allMovies: movieData,
+      allMovies: [],
+      error: ''
     };
+  }
+
+  componentDidMount = () => {
+    fetchApiUrl()
+    .then((data) => this.setState({allMovies: data.movies}))
+    .catch((error) => console.log('error', error));
+    console.log("hello")
   }
 
   render() {
