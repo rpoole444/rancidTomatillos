35d0c11485d94124a657b8de3b9f8de2c903073a
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-02 19:01:09 -0600

    Add header images

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../Header/Header.css";
 
+
 const Header = () => {
   return (
     <header>
       <h1>Rancid Tomatillos</h1>
+      <img className="logo-image" src='tomatillo.png'/>
       <input placeholder="Search" name="search" />
-      <img />
     </header>
   );
 };
