1b0cdc8f99a58418184ba348aa4f7a8d65c43130
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-02 21:03:55 -0600

    Update app fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,7 @@
 import { Component } from "react";
-import { fetchApiUrl } from "./Api";
+import { fetchAllData } from "../Api";
 import Header from "../Header/Header";
 import Library from "../Library/Library";
-// import Movies from "../Movies/Movies";
-// import movieData from "../movieData/movieData";
 import "./App.css";
 
 class App extends Component {
@@ -11,16 +9,16 @@ class App extends Component {
     super();
     this.state = {
       allMovies: [],
-      error: ''
+      error: "",
     };
   }
 
   componentDidMount = () => {
-    fetchApiUrl()
-    .then((data) => this.setState({allMovies: data.movies}))
-    .catch((error) => console.log('error', error));
-    console.log("hello")
-  }
+    fetchAllData()
+      .then((data) => this.setState({ allMovies: data[0].movies }))
+      .catch((error) => console.log("error", error));
+    console.log("hello");
+  };
 
   render() {
     return (
