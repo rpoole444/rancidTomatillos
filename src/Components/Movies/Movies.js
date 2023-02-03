8190e1782d27ea0712dcc1b57c7ea252eb2d020c
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-02 19:04:10 -0600

    Add movie title

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Movies.css'
 
+
 const Movies = ({id, key, poster_path, backdrop_path, 
   title, average_rating, release_date}) => {
     return (
-      <section className=''>
-      <h1>{title}</h1>
-      <img src={poster_path} alt={`${title} movie poster`} />
+      <section className='movie-poster-container'>
+        <img src={poster_path} alt={`${title} movie poster`} />
+        <section className='movie-title'>
+          <h2>{title}</h2>
+          <img className='star-image' src='star.png' alt='star logo' />
+          <h2>{`${average_rating.toFixed(0)}`}</h2>
+        </section>
       </section>
     )
   }
