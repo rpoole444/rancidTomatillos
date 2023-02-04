6c105651e33ec98ca12b3f809cd1d837f82c8fe0
Reid Poole <poole.reid@gmail.com>
2023-02-04 11:29:15 -0600

    Add all details to MovieDetails

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -11,21 +11,37 @@ class MovieDetails extends Component {
   }
   componentDidMount = () => {
     fetchSingleMovie(`movies/${this.props.movieID}`)
-      .then((data) => console.log(data))
+      .then((data) => this.setState({ singleMovie: data.movie }))
       .catch((error) => console.log("error", error));
     console.log("goodBye");
   };
   render() {
     console.log(this.state.singleMovie);
     return (
-      <section className="one-movie">
-        <img
-          src={this.state.singleMovie.backdrop_path}
-          alt={this.state.singleMovie.title}
-        />
-        <h1>{this.state.singleMovie.title}</h1>
-        <p>{this.state.singleMovie.overview}</p>
-      </section>
+      <div>
+        <section className="one-movie">
+          <img
+            src={this.state.singleMovie.backdrop_path}
+            alt={`${this.state.singleMovie.title} - movie backdrop`}
+          />
+        </section>
+        <section className="movie-trailer">
+          <img
+            src={this.state.singleMovie.poster_path}
+            alt={`${this.state.singleMovie.title} - movie trailer`}
+          />
+        </section>
+        <section className="movie-details">
+          <h1>{this.state.singleMovie.title}</h1>
+          <p>{this.state.singleMovie.tagline}</p>
+          <p>{this.state.singleMovie.overview}</p>
+          <p>{`Release Date: ${this.state.singleMovie.release_date}`}</p>
+          <p>{`Film Budget: $${this.state.singleMovie.budget}`}</p>
+          <p>{`Film Revenue: $${this.state.singleMovie.revenue}`}</p>
+          <p>{`Film Runtime: ${this.state.singleMovie.runtime} minutes`}</p>
+          <p>{this.state.singleMovie.genres}</p>
+        </section>
+      </div>
     );
   }
 }
