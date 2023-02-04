a44d28dad0a8aa2def5fd6c50b5983a1f30c9f3a
Reid Poole <poole.reid@gmail.com>
2023-02-04 10:46:39 -0600

    Add API call functions for single and all movies

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,12 +1,12 @@
-const fetchApiUrl = (path) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
-    .then((response) => response.json())
+const fetchAllMovies = () => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`).then(
+    (response) => response.json()
+  );
+};
+const fetchSingleMovie = (path) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`).then(
+    (response) => response.json()
+  );
 };
 
-const fetchAllData = () => {
-  return Promise.all([
-    fetchApiUrl("movies"),
-    fetchApiUrl("movies/436270")]);
-};
-
-export { fetchAllData }
+export { fetchAllMovies, fetchSingleMovie };
