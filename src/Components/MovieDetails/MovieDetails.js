f46fd6be3501c316306950abb7a7ef7df4ea7df0
Reid Poole <poole.reid@gmail.com>
2023-02-06 15:24:18 -0600

    Add background image and parent element container

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -19,31 +19,41 @@ class MovieDetails extends Component {
   render() {
     console.log(this.state.singleMovie);
     return (
-      <section className="one-movie">
-         <NavLink to='/'>
-            <section className='button-container'>
-              <button className='home-button'>Back To Home</button>
-            </section>
-          </NavLink>
-         <section className="movie-title-container">
-            <h1>{this.state.singleMovie.title}</h1>
+      <section
+        className="one-movie"
+        style={{
+          backgroundImage: `url(${this.state.singleMovie.backdrop_path})`,
+        }}
+      >
+        <NavLink to="/">
+          <section className="button-container">
+            <button className="home-button">Back To Home</button>
           </section>
+        </NavLink>
+        <section className="movie-title-container">
+          <h1>{this.state.singleMovie.title}</h1>
+        </section>
         <section className="middle-container">
-        <section className="movie-trailer">
-          <img className="single-movie-poster"
-            src={this.state.singleMovie.poster_path}
-            alt={`${this.state.singleMovie.title} - movie trailer`}
-          />
-        </section>
-        <section className="movie-details">
-          <p>{this.state.singleMovie.tagline}</p>
-          <p className="movie-overview">{this.state.singleMovie.overview}</p>
-          <p>{`Release Date: ${this.state.singleMovie.release_date}`}</p>
-          <p>{`Film Budget: $${this.state.singleMovie.budget} million`}</p>
-          <p>{`Film Revenue: $${this.state.singleMovie.revenue}`}</p>
-          <p>{`Film Runtime: ${this.state.singleMovie.runtime} minutes`}</p>
-          <p>{this.state.singleMovie.genres}</p>
-        </section>
+          <section className="movie-trailer">
+            <img
+              className="single-movie-poster"
+              src={this.state.singleMovie.poster_path}
+              alt={`${this.state.singleMovie.title} - movie trailer`}
+            />
+          </section>
+          <section className="movie-details">
+            <section className="details-container">
+              <p>{this.state.singleMovie.tagline}</p>
+              <p className="movie-overview">
+                {this.state.singleMovie.overview}
+              </p>
+              <p>{`Release Date: ${this.state.singleMovie.release_date}`}</p>
+              <p>{`Film Budget: $${this.state.singleMovie.budget} million`}</p>
+              <p>{`Film Revenue: $${this.state.singleMovie.revenue}`}</p>
+              <p>{`Film Runtime: ${this.state.singleMovie.runtime} minutes`}</p>
+              <p>{this.state.singleMovie.genres}</p>
+            </section>
+          </section>
         </section>
       </section>
     );
