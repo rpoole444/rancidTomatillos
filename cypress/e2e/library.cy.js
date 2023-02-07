dcc45eb31900d7a2fd64d1dcebbf8db26c13cba5
Reid Poole <poole.reid@gmail.com>
2023-02-06 20:41:56 -0600

    Add Cypress tests for library

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("A user should see the list of Movies", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Should connect to localhost site", () => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Should display a Title", () => {
+    cy.get(".movie-container").should("be.visible");
+  });
+});
