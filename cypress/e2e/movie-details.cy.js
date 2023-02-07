e1acb100c45f950313952767340680120ca7fe71
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-07 16:01:03 -0600

    Add movie detail cypress tests for individual page

--- cypress/e2e/movie-details.cy.js
+++ cypress/e2e/movie-details.cy.js
@@ -1,6 +1,26 @@
 describe("User should see a Movie Title, Trailer, Movie Details, and Back Button.", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/724495");
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/:movie_id", () => {
+    return  {
+      id: 724495,
+      title: "The Woman King",
+      poster_path: "https://image.tmdb.org/t/p/original//438QXt1E3WJWb3PqNniK0tAE5c1.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//7zQJYV02yehWrQN6NjKsBorqUUS.jpg",
+      release_date: "2022-09-15",
+      overview: "The story of the Agojie, the all-female unit of warriors who protected the African Kingdom of Dahomey in the 1800s with skills and a fierceness unlike anything the world has ever seen, and General Nanisca as she trains the next generation of recruits and readies them for battle against an enemy determined to destroy their way of life.",
+      genres: [
+      "Action",
+      "Drama",
+      "History"
+      ],
+      budget: 50000000,
+      revenue: 91000000,
+      runtime: 135,
+      tagline: "Her reign begins.",
+      average_rating: 4
+      }
+    })
   });
 
   it("Should connect to specific movie site", () => {
@@ -8,12 +28,36 @@ describe("User should see a Movie Title, Trailer, Movie Details, and Back Button
   });
 
   it("Should See a Movie title", () => {
-    cy.get("h1").should("be.visible");
+    cy.get("h1").should("contain", "The Woman King");
   });
 
-  it("Should See a Movie Trailer", () => {});
+  it("Should See a Movie Trailer", () => {
+    cy.get('iframe')
+    .should('be.visible')
+  });
 
-  it("Should See a Movie Details", () => {});
+  it("Should See a Movie Tagline", () => {
+    cy.get("p").should("contain", "Her reign begins.");
 
-  it("Should See a Back Button", () => {});
+  });
+
+  it("Should See a Movie Overview", () => {
+    cy.get("p").should("contain", "The story of the Agojie, the all-female unit of warriors who protected the African Kingdom of Dahomey in the 1800s with skills and a fierceness unlike anything the world has ever seen, and General Nanisca as she trains the next generation of recruits and readies them for battle against an enemy determined to destroy their way of life.");
+  });
+  
+  it("Should See a Release Date", () => {
+    cy.get("p").should("contain", "2022-09-15");
+  });
+
+  it("Should See a Film Budget", () => {
+    cy.get("p").should("contain", "50000000");
+  });
+
+  it("Should See a Film Revenue", () => {
+    cy.get("p").should("contain", "91000000");
+  });
+
+  it("Should See a Film Runtime", () => {
+    cy.get("p").should("contain", "135");
+  });
 });
