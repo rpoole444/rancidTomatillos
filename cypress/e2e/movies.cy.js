d8b3a9405bdc82051c66695c25814821a2996418
Reid Poole <poole.reid@gmail.com>
2023-02-06 20:45:32 -0600

    Add Cypress Tests for movies

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("User should see a Movie Image, Title, and Rating for each movie in the library", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Should connect to localhost site", () => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("User should see Movie images", () => {
+    cy.get(".poster-image").should("be.visible");
+  });
+
+  it("User should see the Movie titles and ratings", () => {
+    cy.get(".movie-title").should("be.visible");
+  });
+
+  it("User should see a star image", () => {
+    cy.get(".star-image").should("be.visible");
+  });
+});
