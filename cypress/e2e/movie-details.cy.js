d53f733dda99d1bbcea74668dd878cd96a273481
Reid Poole <poole.reid@gmail.com>
2023-02-06 20:43:01 -0600

    Add Cypress tests for Movie-Details WIP

--- cypress/e2e/movie-details.cy.js
+++ cypress/e2e/movie-details.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("User should see a Movie Title, Trailer, Movie Details, and Back Button.", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/724495");
+  });
+
+  it("Should connect to specific movie site", () => {
+    cy.visit("http://localhost:3000/724495");
+  });
+
+  it("Should See a Movie title", () => {
+    cy.get("h1").should("be.visible");
+  });
+
+  it("Should See a Movie Trailer", () => {});
+
+  it("Should See a Movie Details", () => {});
+
+  it("Should See a Back Button", () => {});
+});
