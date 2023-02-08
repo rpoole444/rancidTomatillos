a227f2d21d89811fd0dfa96483ed77306579e92b
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-07 21:00:39 -0600

    Edit catch in App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { fetchAllMovies } from "../Api";
 import Header from "../Header/Header";
 import Library from "../Library/Library";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import MovieDetails from "../MovieDetails/MovieDetails";
 import "./App.css";
 class App extends Component {
@@ -10,18 +10,31 @@ class App extends Component {
     super();
     this.state = {
       allMovies: [],
-      error: "",
+      error: false,
+      loading: false,
     };
   }
   componentDidMount = () => {
+    this.setState({loading: true})
     fetchAllMovies()
-      .then((data) => this.setState({ allMovies: data.movies }))
-      .catch((error) => console.log("error", error));
-    console.log("hello");
+      .then((data) => this.setState({ allMovies: data.movies, loading: false }))
+      .catch((error) => this.getError());
   };
+
+  getError = () => {
+    this.setState({error: true})
+  }
+  
   render() {
     return (
       <main className="App">
+        {this.state.error && 
+         <section className="error-box">
+          <h1 className="error-header">UH OH! Something went wrong. Please try again.</h1>
+          <img className="error-image" src="robot.jpg" alt="robot"></img>
+       </section> 
+        }
+        <Switch>
         <Route
           exact
           path="/"
@@ -38,9 +51,13 @@ class App extends Component {
           exact
           path="/:movieId"
           render={({ match }) => {
-            return <MovieDetails movieID={match.params.movieId} />;
+          return <MovieDetails movieID={match.params.movieId} />;
           }}
         ></Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+        </Switch>
       </main>
     );
   }
