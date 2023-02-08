b52f96eecd0ffe71b1f9af5d86180492f5e949a0
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-07 20:58:32 -0600

    Add tests for header

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -9,15 +9,15 @@ describe("A user should see a Logo, Title, and SearchBar in the Header", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("Should display a Title", () => {
+  it("Should display a title", () => {
     cy.get(".title-logo").contains("Rancid Tomatillos");
   });
 
-  it("Should display a Logo", () => {
+  it("Should display a logo", () => {
     cy.get(".logo-image").should("be.visible");
   });
 
-  it("Should display a search Bar", () => {
+  it("Should display a search bar", () => {
     cy.get("input").should("be.visible");
   });
 });
