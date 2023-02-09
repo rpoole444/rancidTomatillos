edc0b2075589202b2c9438217f42f0b0455eef11
Reid Poole <poole.reid@gmail.com>
2023-02-09 16:33:08 -0600

    Add OnSearch method and call the props method to interact with the value of the search bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,13 +4,17 @@ import "../Header/Header.css";
 const Header = (props) => {
   const { onUpdateSearch } = props;
 
+  const onSearch = (e) => {
+    onUpdateSearch(e.currentTarget.value);
+  };
+
   return (
     <header className="header-bar">
       <section className="title-logo">
         <h1>Rancid Tomatillos</h1>
         <img className="logo-image" src="tomatillo.png" alt="tomatillo" />
       </section>
-      <input placeholder="Search" name="search" />
+      <input onChange={onSearch} placeholder="Search" name="search" />
     </header>
   );
 };
