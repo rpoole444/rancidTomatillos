86c22d8386c58a2b0524d70825f28c5bb4e2b525
Reid Poole <poole.reid@gmail.com>
2023-02-09 14:24:37 -0600

    Add Error Page Route and edit state error properties

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { fetchAllMovies } from "../Api";
 import Header from "../Header/Header";
 import Library from "../Library/Library";
+import ErrorPage from "../ErrorPage/ErrorPage";
 import { Route, Switch, Redirect } from "react-router-dom";
 import MovieDetails from "../MovieDetails/MovieDetails";
 import "./App.css";
@@ -10,53 +11,43 @@ class App extends Component {
     super();
     this.state = {
       allMovies: [],
-      error: false,
       loading: false,
     };
   }
+
   componentDidMount = () => {
-    this.setState({loading: true})
+    this.setState({ loading: true });
     fetchAllMovies()
       .then((data) => this.setState({ allMovies: data.movies, loading: false }))
-      .catch((error) => this.getError());
+      .catch((error) => console.log(error, "Error setting library"));
   };
 
-  getError = () => {
-    this.setState({error: true})
-  }
-  
   render() {
     return (
       <main className="App">
-        {this.state.error && 
-         <section className="error-box">
-          <h1 className="error-header">UH OH! Something went wrong. Please try again.</h1>
-          <img className="error-image" src="robot.jpg" alt="robot"></img>
-       </section> 
-        }
         <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return (
-              <div>
-                <Header />
-                <Library allMovies={this.state.allMovies} />
-              </div>
-            );
-          }}
-        ></Route>
-        <Route
-          exact
-          path="/:movieId"
-          render={({ match }) => {
-          return <MovieDetails movieID={match.params.movieId} />;
-          }}
-        ></Route>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <Header />
+                  <Library allMovies={this.state.allMovies} />
+                </div>
+              );
+            }}
+          ></Route>
+          <Route
+            exact
+            path="/:movieId"
+            render={({ match }) => {
+              return <MovieDetails movieID={match.params.movieId} />;
+            }}
+          ></Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
         </Switch>
       </main>
     );
