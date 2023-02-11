a63da23f85832389a76e458cca57abd09ef6770e
Reid Poole <poole.reid@gmail.com>
2023-02-11 14:03:59 -0600

    Add style to link and page

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -6,7 +6,7 @@ const ErrorPage = () => {
   return (
     <div className="error-page">
       <h1>404 Error</h1>
-      <img src="robot.png" alt="robot" />
+      <img src="logo192.png" alt="robot" />
       <h2>Page Not Found</h2>
       <Link className="link" to="/">
         Go Home
