05c49461ec0b087eefd6cf365e8d820f1d0639b9
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-10 21:48:54 -0600

    Add cypress library tests

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -1,6 +1,7 @@
 describe("A user should see the list of Movies", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+    cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {fixture: "library.json"})
   });
 
   it("Should connect to localhost site", () => {
@@ -8,10 +9,30 @@ describe("A user should see the list of Movies", () => {
   });
 
   it("Should display a title", () => {
-    cy.get(".movie-container").should("be.visible");
+    cy.get('section[class="movie-title"]').should("be.visible");
   });
 
   it("Should display all movies", () => {
-    cy.get(".movie-container").find(".poster-image").should("have.length", "40");
+    cy.get(".movie-container").find(".poster-image").should("have.length", "5");
   });
+
+  it("Should display star image", () => {
+    cy.get(".star-image").should("be.visible");
+  });
+
+  it("Should display search bar", () => {
+    cy.get(".star-image").should("be.visible");
+  });
+
+  // it("should be able to click on a movie and redirec to movie page", () => { 
+  //   cy.get('section[class="movie-poster-container"]').click() 
+  //   cy.url().should('eq', 'http://localhost:3000/436270') 
+  // })
+
+  it('Should redirect to movie page after clicking on movie poster', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.poster-image').first().click(); 
+    cy.location('http://localhost:3000/movies/').should('include', '/436270');
+});
+//should check url against expexted rules using
 });
