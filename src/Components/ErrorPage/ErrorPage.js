d0322e2757280e9861c7589b37ccce772668f9c2
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-12 12:13:25 -0600

    Delete robot image

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -6,7 +6,6 @@ const ErrorPage = () => {
   return (
     <div className="error-page">
       <h1>404 Error</h1>
-      <img src="logo192.png" alt="robot" />
       <h2>Page Not Found</h2>
       <Link className="link" to="/">
         Go Home
